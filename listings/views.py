b5aaf5b284c6db3ae17f7bf035783c9ed774b8ea
vt-park <vincent1234park@gmail.com>
2026-03-15 16:40:46 -0400

    fixed routing

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Listing
+from django.shortcuts import redirect, render
+
 from .forms import ListingForm
+from .models import Listing
+
 
 # Minimal current views for listing-related pages
 def search(request):
@@ -12,24 +14,22 @@ def listing_detail(request):
     return render(request, "listings/listing_detail.html")
 
 
-def create_listing(request):
-    return render(request, "listings/create_listing.html")
-
 def listing_list(request):
-    listings = Listing.objects.filter(is_hidden=False).order_by('-created_at')
-    
-    return render(request, 'listings/listing_list.html', {'listings': listings})
+    listings = Listing.objects.filter(is_hidden=False).order_by("-created_at")
+
+    return render(request, "listings/listing_list.html", {"listings": listings})
+
 
 @login_required
 def create_listing(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ListingForm(request.POST)
         if form.is_valid():
             listing = form.save(commit=False)
-            listing.owner = request.user 
+            listing.owner = request.user
             listing.save()
-            return redirect('listing_list') # Send them back to the feed
+            return redirect("listings:listing_list")  # Send them back to the feed
     else:
         form = ListingForm()
-    
-    return render(request, 'listings/listing_form.html', {'form': form})
+
+    return render(request, "listings/listing_form.html", {"form": form})
